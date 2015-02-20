f978c379a25b61a0b3d86b8be23f09db17326fbf
Albert Albala <albert-albala-not-monitored@mediatribe.net>
2015-02-20 13:48:54 -0500

    test

--- scripts/mirror.sh
+++ scripts/mirror.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-if [ "$#" -ne  "2" ]
+if [ "$#" -ne  "1" ]
   then
-    echo "Please supply exactly two arguments, not $#"
+    echo "Please supply exactly one argument, not $#"
     echo ""
     echo "Usage:"
     echo ""
     echo "Mirrors this git repo from one location to another."
     echo "First make sure you have access to the source and destinations via ssh key."
-    echo "Then call, periodically from continuous integration server:"
+    echo "Then call, (for example) periodically from continuous integration server:"
     echo ""
-    echo "./scripts/mirror.sh git@example.com/source/repo git@example.com/destination/repo"
+    echo "./scripts/mirror.sh git@example.com/destination/repo"
 else
-  git clone --mirror $1 _project_
-  cd _project_
-  git push --mirror $2
-  RETURN=$?
-  cd ..
-  rm -rf _project_
-  exit $RETURN
+  # see http://blog.plataformatec.com.br/2013/05/how-to-properly-mirror-a-git-repository/
+  git fetch --prune
+  git push --prune $1 +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
 fi
